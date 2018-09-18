a8ccd6167dc4d4a7af6b91b0eaa6fd94da3018a2
Hickacou <leflon.paul@gmail.com>
2018-09-18 19:45:09 +0200

    First commit

---
+++ elements.js
@@ -0,0 +1,48 @@
+//Play Button
+console.log("fefe");
+var menuAvatar = document.querySelector(".nav2-header .nav2__col--avatar");
+var theContainer = document.createElement("div");
+theContainer.className = "nav2__col";
+theContainer.id = "theContainer";
+
+var thePlayButton = document.createElement("a");
+thePlayButton.className = "nav2__button nav2__button--stadium";
+thePlayButton.id = "thePlayButton";
+thePlayButton.setAttribute("href","osu://");
+thePlayButton.innerHTML = "<i class='fas fa-gamepad'></i> Play !";
+
+theContainer.appendChild(thePlayButton);
+menuAvatar.insertAdjacentElement("afterend",theContainer);
+
+//Game Launched Overlay 
+
+var overlayContainer = document.createElement("div");
+var osuLogo = document.createElement("img");
+var overlayMessage = document.createElement("div");
+
+overlayContainer.id = "overlayContainer";
+
+osuLogo.src= "https://osu.ppy.sh/images/layout/osu-logo-white.svg";
+osuLogo.id = "osuLogo";
+
+overlayMessage.innerText = "Game started.";
+overlayMessage.id = "overlayMessage";
+
+overlayContainer.appendChild(osuLogo)
+overlayContainer.appendChild(overlayMessage);
+document.body.appendChild(overlayContainer);
+
+thePlayButton.addEventListener("click",() => {
+    overlayContainer.setAttribute("class","active");
+    setTimeout(() => overlayContainer.removeAttribute("class"),2500);
+});
+
+var oldLocation = location.href;
+setInterval(function() {
+    if (oldLocation != location.href) {
+        console.log("new :pog:");
+        oldLocation = location.href;
+        // menuAvatar.insertAdjacentElement("afterend",theContainer);
+        location.reload();
+    }
+},100);
